360ac94a23a1accac43d22f441e5b9df7abfdf84
prabal dwivedi <prabaldwivedi2907@gmail.com>
2026-07-09 15:52:21 +0530

    Updating index js file

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,8 @@ import express from "express";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import cookieParser from "cookie-parser";
-import cors from "cors"
-import dotenv from "dotenv"
+import cors from "cors";
+import dotenv from "dotenv";
 import connectDB from "./db.js";
 import router from "./authroutes.js";
 dotenv.config();
@@ -12,31 +12,30 @@ connectDB();
 
 const app = express();
 
-// app.use(
-//   cors({
-//     origin: [
-//       "http://localhost:3000",
-//       "https://netflix-gpt-clone-tau.vercel.app",
-//     ],
-//     credentials: true,
-//   }),
-// );
-app.use(cors({origin:"*"}))
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "https://netflix-gpt-clone-tau.vercel.app",
+    ],
+    credentials: true,
+  }),
+);
+
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+app.get("/", (req, res) => {
+  console.log("hey how are you??");
+  return res.status(201).json({
+    message: "heyy from the server",
+    success: true,
+  });
+});
 
-app.get("/",(req,res)=>{
-    console.log("hey how are you??");
-    return res.status(201).json({
-        message:"heyy from the server",
-        success:true
-    })
-})
+app.use("/api/user", router);
 
-app.use("/api/user",router);
-
-app.listen(5000,()=>{
-    console.log("Sever is running")
-})
+app.listen(5000, () => {
+  console.log("Sever is running");
+});
